4c43f4a16650f99b6bfaa7a3a4bdee4a3ef5fcab
Aruna <aruna.evan15@gmail.com>
2022-02-01 13:31:48 -0800

    Solution

--- javascript/distinct_pair_sum.js
+++ javascript/distinct_pair_sum.js
@@ -1,9 +1,44 @@
 function distinctPairSum(arr, k) {
-  // type your code here
+  let res = [];
+  for (let i=0;i<arr.length-2;i++){
+    if (arr[i] + arr[i+1] === k) {
+      res.push([arr[i], arr[i+1]]);
+    }
+  }
+
+  return res;
+
+  // Misunderstood problem as *ANY* pairing (solution below).
+
+  // const dict = {};
+
+  // arr.forEach(el=>{
+  //   if (dict[el] && dict[el]<2){
+  //     dict[el]++;
+  //   }
+  //   else {
+  //     dict[el] = 1;
+  //   }
+  // })
+
+  // const res = []
+  // for (const el in dict){
+  //   if (dict[el] === 1){
+  //     delete dict[el];
+  //   }
+  //   else {
+  //     dict[el]--;
+  //   }
+
+  //   if (dict[k-el]){
+  //     res.push([el, k-el]);
+  //     delete dict[k-el];
+  //   }
+  // }
+  // return res;
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: [[1, 1], [2, 0]]");
   console.log("=>", distinctPairSum([0, 1, 1, 2, 0, 1, 1], 2));
 
@@ -15,5 +50,23 @@ if (require.main === module) {
 
 module.exports = distinctPairSum;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/**
+ * 1. paraphrase
+ * 
+ * given an array and k, return array of arrays of distinct CONSECUTIVE pairs that sum to k
+ * 
+ * 2. tests
+ * 
+ * 3. pseudocode
+ * 
+ * pop() an element, subtract from k.
+ * iterate over array. splice if matching the popped value, else check if matching.
+ * 
+ * O(n^2)
+ * 
+ * new plan: place all elements in a set. Then O(2n).
+ * 
+ * 4. code
+ * 5. refactor
+ * 6. optimize if time
+ */
